e47b0a601aecdad68c68ec96bcbde46d743611c9
andreastedile <andrea.stedile@studenti.unitn.it>
2022-01-18 21:54:53 +0100

    Refactor

--- src/main/java/it/unitn/ds2/gui/components/ApplicationContext.java
+++ src/main/java/it/unitn/ds2/gui/components/ApplicationContext.java
@@ -1,24 +1,20 @@
 package it.unitn.ds2.gui.components;
 
 import akka.actor.typed.ActorSystem;
-import akka.actor.typed.Behavior;
-import it.unitn.ds2.raft.Raft;
 
 public class ApplicationContext {
     public final CommandBus commandBus;
     public final EventBus eventBus;
 
     public ApplicationContext() {
-        Behavior<Raft> raftBehavior = SimulationController.create(this);
-        ActorSystem<Raft> raftActorSystem = ActorSystem.create(raftBehavior, "raft-cluster");
-
-        commandBus = new CommandBus(raftActorSystem);
+        var actorSystem = ActorSystem.create(SimulationController.create(this), "raft-cluster");
+        commandBus = new CommandBus(actorSystem);
         eventBus = new EventBus();
 
     }
 
     /**
-     * Terminates all the it.unitn.ds2.gui.components of the application.
+     * Terminates all the components of the application.
      */
     public void terminate() {
         commandBus.terminate();
