8fdc34910f485be88457334bec816ac06b6e4d34
Matteo Franzil <matteofranzil@gmail.com>
2022-01-19 15:23:22 +0100

    Close the window -> terminate the process

--- src/main/java/it/unitn/ds2/gui/App.java
+++ src/main/java/it/unitn/ds2/gui/App.java
@@ -3,8 +3,11 @@ package it.unitn.ds2.gui;
 import it.unitn.ds2.gui.components.ApplicationContext;
 import it.unitn.ds2.gui.view.MainView;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class App extends Application {
@@ -20,6 +23,10 @@ public class App extends Application {
         var mainView = new MainView(applicationContext);
         var scene = new Scene(mainView, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+        primaryStage.setOnCloseRequest(event -> {
+            Platform.exit();
+            System.exit(0);
+        });
         primaryStage.setTitle("Raft cluster simulator");
         primaryStage.setScene(scene);
 
