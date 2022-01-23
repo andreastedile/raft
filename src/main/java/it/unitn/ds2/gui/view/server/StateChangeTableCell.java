c4a91b11cfe4cb2e19d8aa0e155192394ad8bca5
andreastedile <andrea.stedile@studenti.unitn.it>
2022-01-23 12:01:39 +0100

    Crash button: remove duration feature

--- src/main/java/it/unitn/ds2/gui/view/server/StateChangeTableCell.java
+++ src/main/java/it/unitn/ds2/gui/view/server/StateChangeTableCell.java
@@ -1,15 +1,12 @@
 package it.unitn.ds2.gui.view.server;
 
 import it.unitn.ds2.gui.commands.CrashServer;
+import it.unitn.ds2.gui.commands.RestartServer;
 import it.unitn.ds2.gui.components.ApplicationContext;
 import it.unitn.ds2.gui.model.ServerModel;
 import it.unitn.ds2.raft.events.StateChange;
-import it.unitn.ds2.raft.properties.SimulationProperties;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
-
-import java.time.Duration;
-import java.util.concurrent.ThreadLocalRandom;
 
 
 public class StateChangeTableCell<T> extends TableCell<T, ServerModel> {
@@ -35,33 +32,24 @@ public class StateChangeTableCell<T> extends TableCell<T, ServerModel> {
                 if (item.getServer().equals(stateChangeEvent.publisher)) {
                     switch (stateChangeEvent.state) {
                         case OFFLINE -> {
-                            button.setText("Offline");
-                            button.setDisable(true);
-                            //button.setOnAction(event -> {
-                            //    var command = new RestartServer(item.getServer());
-                            //    applicationContext.commandBus.emit(command);
-                            //});
+                            button.setText("Resume");
+                            button.setDisable(false);
+                            button.setOnAction(event -> {
+                                var command = new RestartServer(item.getServer());
+                                applicationContext.commandBus.emit(command);
+                            });
                         }
                         case FOLLOWER, LEADER, CANDIDATE -> {
                             button.setText("Crash");
                             button.setDisable(false);
                             button.setOnAction(event -> {
-                                var duration = randomCrashDuration();
-                                var command = new CrashServer(item.getServer(), duration);
+                                var command = new CrashServer(item.getServer(), null);
                                 applicationContext.commandBus.emit(command);
-                                button.setText("Crashed (" + duration.toMillis() + "s)");
-                                button.setDisable(true);
                             });
                         }
                     }
                 }
             });
         }
-    }
-
-    private static Duration randomCrashDuration() {
-        var properties = SimulationProperties.getInstance();
-        long durationMs = ThreadLocalRandom.current().nextLong(properties.maxCrashDurationMs);
-        return Duration.ofSeconds(durationMs);
     }
 }
